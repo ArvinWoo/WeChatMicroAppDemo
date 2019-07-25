d1c52d2216f412de18616a8bd82d8ed4001441b7
ArvinWoo <www.1328153938@qq.com>
2019-07-26 00:53:33 +0800

    -- 初次导入

---
+++ pages/login/login.js
@@ -0,0 +1,109 @@
+// pages/login/login.js
+var types = ['default', 'primary', 'warn']
+var pageObject = {
+  data: {
+    defaultSize: 'default',
+    primarySize: 'default',
+    warnSize: 'default',
+    disabled: false,
+    plain: false,
+    loading: false
+  },
+  setDisabled: function (e) {
+    this.setData({
+      disabled: !this.data.disabled
+    })
+  },
+  setPlain: function (e) {
+    this.setData({
+      plain: !this.data.plain
+    })
+  },
+  setLoading: function (e) {
+    this.setData({
+      loading: !this.data.loading
+    })
+  },
+  onGotUserInfo: function (e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.userInfo)
+    console.log(e.detail.rawData)
+  },
+}
+
+for (var i = 0; i < types.length; ++i) {
+  (function (type) {
+    pageObject[type] = function (e) {
+      var key = type + 'Size'
+      var changedData = {}
+      changedData[key] =
+        this.data[key] === 'default' ? 'mini' : 'default'
+      this.setData(changedData)
+    }
+  })(types[i])
+}
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/site/search.js
@@ -0,0 +1,134 @@
+//search.js  
+//获取应用实例  
+var app = getApp();
+Page({
+  data: {
+    /** 
+    * 页面配置 
+    */
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换  
+    currentTab: 0,
+    goto:'../images/site/search/goto.png',//lineSearch 图标
+    searchLinePlaceholder:'请输入周边范围',//lineSearch input 提示
+    styleVisibility:'visShow',//lineSearch 公里 Style属性
+    Tab1:'',
+    Tab2:'disNone',
+    markers: [{
+      iconPath:"../imges/site/zaijian.png",
+      iconZaiJian: "../imges/site/zaijian.png",
+      iconTuoYuan: "../imges/site/tuoyuan.png",
+      iconChouJian: "../imges/site/choujian.png",
+      iconDingWei: "../imges/site/dingwei.png",
+      id: 0,
+      latitude: 40.002607,
+      longitude: 116.487847,
+      width: 35,
+      height: 45
+    }],
+  },
+  onLoad: function () {
+    var that = this;
+    /** 
+    * 获取系统信息 
+    */
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    });
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        console.log("获取位置信息成功：");
+        console.log(JSON.stringify(res));
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          speed: res.speed,
+          accuracy: res.accuracy,
+        });
+      },
+      fail: function (res) {
+        console.log(res)
+      },
+      complete: function (res) {
+        console.log('Get Position Complete')
+      }
+    });
+  },
+  /** 
+  * 滑动切换tab 
+  */
+  bindChange: function (e) {
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+  },
+  /** 
+   * 按周边搜索
+   */
+  swichAround: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current,
+        searchLinePlaceholder: '请输入周边范围',
+        styleVisibility: 'visShow',
+        Tab2:'disNone',
+        Tab1:'',
+      }) 
+    }
+  },
+   /** 
+   * 按具体工地搜索
+   */
+  swichSpecific: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current,
+        searchLinePlaceholder: '按工地编码、工地名称查询',
+        styleVisibility: 'visHide',
+        Tab1: 'disNone',
+        Tab2: '',
+      })
+    }
+  },
+  dingwei:function(e){
+    var that = this;
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        console.log("获取位置信息成功：");
+        console.log(JSON.stringify(res));
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          speed: res.speed,
+          accuracy: res.accuracy,
+        });
+      },
+      fail: function (res) {
+        console.log(res)
+      },
+      complete: function (res) {
+        console.log('Get Position Complete')
+      }
+    });
+    // 消息组件 toast
+    // wx.showToast({
+    //   title: '成功',
+    //   icon: 'success',
+    //   duration: 2000
+    // })
+  }
+
+})  
